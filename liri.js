69a576427cccb7492342af969b2359ebd43c0dae
Brian Manchester <brianmanchester@brians-imac.attlocal.net>
2017-04-25 14:20:11 -0400

    done

--- liri.js
+++ liri.js
@@ -7,6 +7,16 @@ var twitterClient = new twitter(myKeys.twitterKeys);
 var firstInput = process.argv[2];
 var allInputs = process.argv;
 var title = "";
+//The following lOne etc. variables were made to assist in appending the data responses to the log.txt. These variables allow me to use the response data without having to type the JSON .notation every time.
+var lOne = "";
+var lTwo = "";
+var lThree = "";
+var lFour = "";
+var lFive = "";
+var lSix = "";
+var lSeven = "";
+var lEight = "";
+var b = "\n";
 
 switch (firstInput) {
 	case "my-tweets":
@@ -27,6 +37,17 @@ switch (firstInput) {
 		} else {
 			plusConcat();
 		}
+		break;
+
+	case "do-what-it-says":
+		fs.readFile("random.txt", "utf8", function(err, data) {
+			var dataArray = data.split(",");
+			firstInput = dataArray[0];
+			allInputs = dataArray[1].replace(/"/g, "").split(" ");
+			allInputs.unshift("", "", "");
+			plusConcat(allInputs);
+		});
+		break;
 }
 
 function plusConcat() {
@@ -37,7 +58,7 @@ function plusConcat() {
 			title = allInputs[i];
 		}
 	}
-	console.log(title);
+
 	if (firstInput === "spotify-this-song") {
 		song(title);
 	} else {
@@ -46,54 +67,108 @@ function plusConcat() {
 }
 
 function tweets() {
+	//I could have written a bunch of tweets, but I decided to post someone else's since they're more interesting.
 	twitterClient.get("statuses/user_timeline", { screen_name: "StephenAtHome", count: "20" }, function(err, tweets, response) {
-	 	for (var i = 0; i < 20; i++) {
-	 		console.log("TWEET #" + (i + 1) + " - " + tweets[i].text + " CREATED: " + tweets[i].created_at);
-	 	}
+	 	if (!err) {
+		 	for (var i = 0; i < 20; i++) {
+		 		console.log("TWEET #" + (i + 1) + " - " + tweets[i].text + " CREATED: " + tweets[i].created_at);
+
+		 		fs.appendFile("log.txt", firstInput + " result #" + (i + 1) + b + "TWEET #" + (i + 1) + " - " + tweets[i].text + " CREATED: " + tweets[i].created_at + b + b, function(err) {
+					if (err) {
+						console.log(err);
+					}
+				});
+		 	}
+		} else {
+			console.log(err);
+		}
 	});
 }
 
 function ace() {
 	spotify.search({ type: "track", query: "The+Sign" }, function(err, data) {
-		var res = data.tracks.items[3];
-		console.log("Artist(s): " + res.album.artists[0].name);
-		console.log("Song title: " + res.name);
-		console.log("Album title: " + res.album.name);
-		console.log("Link to preview at Spotify: " + res.preview_url);
+		if (!err) {
+			var res = data.tracks.items[3];
+			lOne = "Artist(s): " + res.album.artists[0].name;
+			lTwo = "Song title: " + res.name;
+			lThree = "Album title: " + res.album.name;
+			lFour = "Link to preview at Spotify: " + res.preview_url;
+
+			console.log(lOne);
+			console.log(lTwo);
+			console.log(lThree);
+			console.log(lFour);
+
+			fs.appendFile("log.txt", firstInput + " 'The Sign'" + b + lOne + b + lTwo + b + lThree + b + lFour + b + b, function(err) {
+				if (!err) {
+					console.log(err);
+				}
+			});
+		} else {
+			console.log(err);
+		}
 	});
 }
 
 function song(input) {
 	spotify.search({ type: "track", query: input }, function(err, data) {
-		var res = data.tracks.items[0];
-		console.log("Artist(s): " + res.album.artists[0].name);
-		console.log("Song title: " + res.name);
-		console.log("Album title: " + res.album.name);
-		console.log("Link to preview at Spotify: " + res.preview_url);
-		// var trackuri = data.tracks.items[0].uri;
-		// console.log(trackuri);
+		if (!err) {
+			//returning the first three related songs in the array because the user might not know the exact song title, and this will give options
+			for (var i = 0; i < 3; i++) {
+				var res = data.tracks.items[i];
+				lOne = "Artist(s): " + res.album.artists[0].name;
+				lTwo = "Song title: " + res.name;
+				lThree = "Album title: " + res.album.name;
+				lFour = "Link to preview at Spotify: " + res.preview_url;
+				console.log(lOne);
+				console.log(lTwo);
+				console.log(lThree);
+				console.log(lFour);
 
-		// spotify.lookup({ type: "track", id: "6e40mgJiCid5HRAGrbpGA6" }, function (err, data) {
-		// 	console.log(data);
-		// });
+				//I tried using title.replace(/+/g, " ") below, but I kept getting an error. It seems the replace method doesn't like a plus sign even if there is one in the string.
+				fs.appendFile("log.txt", firstInput + " " + title + " result #" + (i + 1) + b + lOne + b + lTwo + b + lThree + b + lFour + b + b, function(err) {
+					if (err) {
+						console.log(err);
+					}
+				});
+			}
+		} else {
+			console.log(err);
+		}
 	});
 }
 
 function movie(input) {
 	var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&r=json&tomatoes=true";
 	request(queryUrl, function(err, response, body) {
+		//I decided to stick to returning one movie since the user is more likely to know the movie title
 		if (!err && response.statusCode === 200) {
 			var res = JSON.parse(body);
-			console.log("Title: " + res.Title);
-			console.log("Year: " + res.Year);
-			console.log("IMDB Rating: " + res.imdbRating);
-			console.log("Country or Countries of Production: " + res.Country);
-			console.log("Language(s): " + res.Language);
-			console.log("Plot: " + res.Plot);
-			console.log("Actors: " + res.Actors);
-			console.log("Rotten Tomatoes Link: " + res.tomatoURL);
+			lOne = "Title: " + res.Title;
+			lTwo = "Year: " + res.Year;
+			lThree = "IMDB Rating: " + res.imdbRating;
+			lFour = "Country or Countries of Production: " + res.Country;
+			lFive = "Language(s): " + res.Language;
+			lSix = "Plot: " + res.Plot;
+			lSeven = "Actors: " + res.Actors;
+			lEight = "Rotten Tomatoes Link: " + res.tomatoURL;
+
+			console.log(lOne);
+			console.log(lTwo);
+			console.log(lThree);
+			console.log(lFour);
+			console.log(lFive);
+			console.log(lSix);
+			console.log(lSeven);
+			console.log(lEight);
+
+			fs.appendFile("log.txt", firstInput + " " + title + b + lOne + b + lTwo + b + lThree + b + lFour + b + b, function(err) {
+				if (err) {
+					console.log(err);
+				}
+			});
+		} else {
+			console.log(err);
 		}
 	});
 }
-
-
